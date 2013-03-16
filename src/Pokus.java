6fc53d89b82502e7305b15bde532897141605502
popo <reznorian@seznam.cz>
2013-03-16 01:07:04 +0100

    jen tak na zkousku

---
+++ src/Pokus.java
@@ -0,0 +1,36 @@
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Pokus {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try{
+		digest("a,12450sasspjmxcvmnvpúxcjncviovxadda54664as45545");
+	}catch (NoSuchAlgorithmException e){
+		System.exit(1);
+	}
+	catch (UnsupportedEncodingException e){
+		System.exit(1);
+	}
+
+	}
+
+	public static void digest(String password) throws NoSuchAlgorithmException, UnsupportedEncodingException{
+		
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        digest.update(password.getBytes("UTF-8"));
+		
+        byte byteArr[] = digest.digest();			
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < byteArr.length; i++) {
+        	sb.append(Integer.toString((byteArr[i] & 0xff) + 0x100, 16).substring(1));
+        	
+        	
+        }
+        System.out.println(sb.toString());
+    }
+}
